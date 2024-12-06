e5d5e4045dcfe91d764444dd4b4057f15ba13c9d
Kaushikk52 <kaushikkarnik635@gmail.com>
2024-12-06 14:36:17 +0530

    minor changes

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -146,8 +146,8 @@ export default function Home() {
       </section>
 
       <section className="py-20 bg-black text-white">
-        <div className="container mx-auto px-4">
-          <h2 className="text-4xl font-bold mb-16">WHAT MATTERS THE MOST</h2>
+        <div className="container mx-auto px-4  ">
+          <h2 className="text-4xl font-bold mb-16 inset-0 bg-[url(/glitch-bg.png)] bg-no-repeat bg-cover text-transparent bg-clip-text">WHAT MATTERS THE MOST</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
             {[
               {
